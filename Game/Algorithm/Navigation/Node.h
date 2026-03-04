bf84013ff46737b239808f329ce571423c720827
lelana0824 <lelana0824@gmail.com>
2026-03-04 10:34:35 +0900

    A* 알고리즘 게임 엔진에 가져옴

---
+++ Game/Algorithm/Navigation/Node.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Math/Vector2.h"
+
+using namespace Wanted;
+
+class Node
+{
+
+public:
+	Node(int x, int y, Node* parentNode = nullptr)
+		:position(x, y), parentNode(parentNode)
+	{
+
+	}
+	Vector2 operator-(const Node& other) const
+	{
+		return Vector2(position.x - other.position.x,
+			position.y - other.position.y);
+	}
+
+	bool operator==(const Node& other) const
+	{
+		return position.x == other.position.x
+			&& position.y == other.position.y;
+	}
+
+public:
+	Vector2 position;
+
+	float gCost = 0.0f;
+
+	float hCost = 0.0f;
+
+	float fCost = 0.0f;
+
+	Node* parentNode = nullptr;
+};
